835510c97d9a7d3761894cb1e267def12ea27ec1
LeonardoMinniti <33843867+LeonardoMinniti@users.noreply.github.com>
2018-04-17 19:16:39 +0200

    Arrow and Physics for the ball
    Arrow For direction and Physics for the ball, collider are now on the floor and not anymore at the background

---
+++ BrickHunter/Assets/Scripts/BallStop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStop : MonoBehaviour {
+
+    public Rigidbody2D ball;
+    public BallController ballControl;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            //Stopping the Ball
+            ball.velocity = Vector2.zero;
+            //Set the ball as active
+            ballControl.canInteract = true;
+        }
+    }
+}
